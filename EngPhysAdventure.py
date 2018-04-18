e3d88141fb4194a1f80c3c59b0fb8898025340a3
kashakt <38477272+kashakt@users.noreply.github.com>
2018-04-18 00:02:45 -0400

    Add files via upload

--- EngPhysAdventure.py
+++ EngPhysAdventure.py
@@ -1,6 +1,22 @@
 #Developer testing 
 from GameFunctions import *
 
+print "============================================================================"
+print "Hello, and welcome to..."
+print "The Great Engineering Text Based Adventure!"
+print "A campus full of interesting characters awaits\n"
+print "But before you go any further there are some things you must know..."
+print "Your orientation never changes. When you enter you will be facing the"
+print "entrance of JHE and you will always face that way."
+print "The commands which allow you to interact with your environment are:"
+print "forward = f, backward = b, left = l, right = r, up = u, down = d"
+print "talk = (person name), attack = (person name), inspect = (item name)"
+print "Typing stats will show your stats"
+print "Typing search will give you an idea of your surroundings"
+print "Typing an item/person to interact with type the ENTIRE name as you read it."
+print "Thats it. Good luck!"
+print "============================================================================"
+
 x = PLAYER.location[0]
 y = PLAYER.location[1]
 z = PLAYER.location[2]
