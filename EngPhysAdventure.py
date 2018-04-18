2c116484204621439fff4db984671de73572230f
kashakt <38477272+kashakt@users.noreply.github.com>
2018-04-18 00:51:01 -0400

    Add files via upload

--- EngPhysAdventure.py
+++ EngPhysAdventure.py
@@ -1,21 +1,28 @@
 #Developer testing 
 from GameFunctions import *
 
-print "============================================================================"
-print "Hello, and welcome to..."
-print "The Great Engineering Text Based Adventure!"
-print "A campus full of interesting characters awaits\n"
+print "=================================================================================================="
+print "Hello, and welcome to...\n"
+print "The Great..."
+print "___________                __________.__"                 
+print "\_   _____/ ____    ____   \______   \  |__ ___.__. ______"
+print " |    __)_ /    \  / ___\   |     ___/  |  <   |  |/  ___/"
+print " |        \   |  \/ /_/  >  |    |   |   Y  \___  |\___ \ "
+print "/_______  /___|  /\___  /   |____|   |___|  / ____/____  >"
+print "        \/     \//_____/                  \/\/         \/ "
+print "Text Based Adventure.\n"
+print "A campus full of interesting characters awaits"
 print "But before you go any further there are some things you must know..."
 print "Your orientation never changes. When you enter you will be facing the"
 print "entrance of JHE and you will always face that way."
 print "The commands which allow you to interact with your environment are:"
 print "forward = f, backward = b, left = l, right = r, up = u, down = d"
 print "talk = (person name), attack = (person name), inspect = (item name)"
-print "Typing stats will show your stats"
-print "Typing search will give you an idea of your surroundings"
+print "Typing stats will show your stats (ATTACK/DEFENCE/SPEED)."
+print "Typing search will give you an idea of your surroundings."
 print "Typing an item/person to interact with type the ENTIRE name as you read it."
 print "Thats it. Good luck!"
-print "============================================================================"
+print "=================================================================================================="
 
 x = PLAYER.location[0]
 y = PLAYER.location[1]
