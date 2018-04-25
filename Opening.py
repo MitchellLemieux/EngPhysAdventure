4670d52ab91b01001b57879e811879187de4610d
MitchellLemieux <38477222+MitchellLemieux@users.noreply.github.com>
2018-04-25 19:02:32 -0400

    Add files via upload

--- Opening.py
+++ Opening.py
@@ -1,56 +1,61 @@
 import time
+DELAY = 1.5
 
-print "                A____ ________"
-print "                /_  H|\_____  \ "
-print "                 |  O|  ___|  |"
-print "                 |  L| /___   <"
-print "                 |  L|  ___\   |"
-print "                 |  Y| /W O O D|"
-print "                 |___|/________/"
-print "                      Production."
-time.sleep(2)
-print ""
-print "         A short time ago on a campus not so far,"
-print "         far away....\n"
-time.sleep(3)
-print "___________                __________.__"                 
-print "\_   _____/ THE_GREAT_____ \______   \  |__ ___.__. ______"
-print " |    __)_ /    \  / ___  > |     ___/  |  \   |  |/  ___/"
-print " |        \   |  \/ /_/  /  |    |   |      \___  |\___ \ "
-print "/_______  /___|  /\___  /   |____|   |___|  / ____/____  >"
-print "        \/     \//_____/  TEXT ADVENTURE  \/\/ (v4.20) \/ "
-time.sleep(3)
-print ""
-print "T h e  c a m p u s  i s  i n  a  s t a t e  o f  u n r e s t."
-time.sleep(1.5)
-print "A n  a n c i e n t  f o r c e  h a s  b e e n  a w o k e n"
-time.sleep(1.5)
-print "a f t e r  t h e  e v e n t s  o f  a  d e b a u c h e r o u s"
-time.sleep(1.5)
-print "e v e n i n g  a t  t h e  P h o e n i x.\n"
-time.sleep(1.5)
-print "O u r  h e r o  a w a k e n s  i n  f r o n t  o f  J H E"
-time.sleep(1.5)
-print "w i t h  a  c o n s i d e r a b l e  h e a d a c h e  b u t"
-time.sleep(1.5)
-print "w i t h o u t  t h e i r  I R O N  R I N G.\n"
-time.sleep(1.5)
-print "C l u e s  a b o u t  l a s t  n i g h t  l i t t e r  t h e"
-time.sleep(1.5)
-print "c a m p u s.  N o  s t o n e  c a n  b e  l e f t  u n t u r n e d."
-time.sleep(1.5)
-print "I t  w i l l  t r u l y  b e  a  t e s t  o f  s k i l l  a s"
-time.sleep(1.5)
-print "w e l l  a s  w i t s  t o  s o l v e  t h i s  m y s t e r y.\n"
-time.sleep(1.5)
-print "T h e  h e r o  m u s t  u n c o v e r  t h e  t r u t h"
-time.sleep(1.5)
-print "a b o u t  l a s t  n i g h t  i f  t h e y  a r e  t o"
-time.sleep(1.5)
-print "h a v e  a n y  h o p e  o f  r e t r i e v i n g  t h e i r"
-time.sleep(1.5)
-print "I R O N  R I N G  a n d  r e t u r n i n g  b a l a n c e"
-time.sleep(1.5)
-print "t o  t h e  f a c u l t y.\n"
-time.sleep(1.5)
+def Opening():
+    print "                A____ ________"
+    print "                /_  H|\_____  \ "
+    print "                 |  O|  ___|  |"
+    print "                 |  L| /___   <"
+    print "                 |  L|  ___\   |"
+    print "                 |  Y| /W O O D|"
+    print "                 |___|/________/"
+    print "                      Production."
+    time.sleep(2)
+    print ""
+    print "         A short time ago on a campus not so far,"
+    print "         far away....\n"
+    time.sleep(3)
+    print "___________                __________.__"                 
+    print "\_   _____/ THE_GREAT_____ \______   \  |__ ___.__. ______"
+    print " |    __)_ /    \  / ___  > |     ___/  |  \   |  |/  ___/"
+    print " |        \   |  \/ /_/  /  |    |   |      \___  |\___ \ "
+    print "/_______  /___|  /\___  /   |____|   |___|  / ____/____  >"
+    print "        \/     \//_____/  TEXT ADVENTURE  \/\/ (v4.20) \/ "
+    time.sleep(DELAY*2)
+    print ""
+    print "T h e  c a m p u s  i s  i n  a  s t a t e  o f  u n r e s t."
+    time.sleep(DELAY)
+    print "A n  a n c i e n t  f o r c e  h a s  b e e n  a w o k e n"
+    time.sleep(DELAY)
+    print "a f t e r  t h e  e v e n t s  o f  a  d e b a u c h e r o u s"
+    time.sleep(DELAY)
+    print "e v e n i n g  a t  t h e  P h o e n i x.\n"
+    time.sleep(DELAY)
+    print "O u r  h e r o  a w a k e n s  i n  f r o n t  o f  J H E"
+    time.sleep(DELAY)
+    print "w i t h  a  c o n s i d e r a b l e  h e a d a c h e  b u t"
+    time.sleep(DELAY)
+    print "w i t h o u t  t h e i r  I R O N  R I N G.\n"
+    time.sleep(DELAY)
+    print "C l u e s  a b o u t  l a s t  n i g h t  l i t t e r  t h e"
+    time.sleep(DELAY)
+    print "c a m p u s.  N o  s t o n e  c a n  b e  l e f t  u n t u r n e d."
+    time.sleep(DELAY)
+    print "I t  w i l l  t r u l y  b e  a  t e s t  o f  s k i l l  a s"
+    time.sleep(DELAY)
+    print "w e l l  a s  w i t s  t o  s o l v e  t h i s  m y s t e r y.\n"
+    time.sleep(DELAY)
+    print "T h e  h e r o  m u s t  u n c o v e r  t h e  t r u t h"
+    time.sleep(DELAY)
+    print "a b o u t  l a s t  n i g h t  i f  t h e y  a r e  t o"
+    time.sleep(DELAY)
+    print "h a v e  a n y  h o p e  o f  r e t r i e v i n g  t h e i r"
+    time.sleep(DELAY)
+    print "I R O N  R I N G  a n d  r e t u r n i n g  b a l a n c e"
+    time.sleep(DELAY)
+    print "t o  t h e  f a c u l t y.\n"
+    time.sleep(DELAY)
+
+def Closing():
+    pass
 
